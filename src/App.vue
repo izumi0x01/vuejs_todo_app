bbd27692b865e46e0125cdc86f8a96695b250228
root <root@Soh-Desktop-PC.localdomain>
2021-12-18 23:53:55 +0900

    first_commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="TodoList">
+    <TodoItem />
+  </div>
+</template>
+<script>
+  import TodoItem from './components/TodoItem.vue'
+
+  export default{
+    name: 'TodoList',
+    components: {
+      TodoItem
+    },
+    data: function(){
+      return{
+        todos: [],
+      }
+    },
+    methods:{
+      AddTodo: function(){
+        this.todos.push({content: this.todo})
+        this.todo=''
+      },
+      RemoveTodo: function(index){
+        this.todos.splice(index, 1)
+      }
+    }
+  } 
+</script>
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div todo="TodoItem">
+    <li>{{todo}}</li>
+  </div>
+</template>
+<script>
+
+let TodoItem = {
+  props: {
+    todo: String
+  },
+  data: function(){
+    return{
+      todo: ""
+      }
+  },
+}
+
+new Vue({
+  name: "TodoItem",
+  "todo-item": TodoItem
+})
+</script>
